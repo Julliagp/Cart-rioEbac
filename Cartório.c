c1dc8a6d6c193c38be9b0be8f489d406a9db45b1
Julliagp <julliagp2005@gmail.com>
2024-07-07 21:49:24 -0300

    Opção de saída
    Incluso no sistema a opção de saída.

--- Cartório.c
+++ Cartório.c
@@ -62,8 +62,10 @@ int registro()
 	fclose(file); //fecha a pasta
 	
 	printf("Registro feito com sucesso!!");
+	printf("Deseja registrar novemente?");
 	
-	system("pause");
+	
+    system("pause");
 	
 }
 
@@ -112,6 +114,7 @@ int deletar()
 		printf("O usuário não se encontra no sitema!.\n"); //mensagem de retorno para o usuário
 		system("pause");
 	}
+	
 }  
 
 int main()
@@ -131,6 +134,7 @@ int main()
 	   printf("\t1 - Registrar nomes\n");
 	   printf("\t2 - Consultar nomes\n");
 	   printf("\t3 - Deletar nomes\n\n");
+	   printf("\t4 - Sair do sistema\n\n");
        printf("Opção: "); //Fim do menu
 	
 	   scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -150,7 +154,12 @@ int main()
 		  case 3:
 		    deletar();
 		  break;
-			 
+		  
+		  case 4:
+		  	printf("Obrigado por utilizar o sistema!"); // sair do sistema 
+		  	return 0;
+		  break;
+		  
 		  default:
 		    printf("Essa opção não está disponível!Tentar novamente no menu.\n");
 			system("pause");
